9e12fb602e8f3ad5cb027cab7b48f72845ac2cef
kuro <anhddse02700@gmail.com>
2023-08-11 18:55:05 +0900

    fix content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog"
-export const SITE_DESCRIPTION = "Welcome to my website!"
+export const SITE_TITLE = "A ♡ A・Wedding !"
+export const SITE_DESCRIPTION =
+  "Hãy cùng chia sẻ niềm hạnh phúc trong lễ cưới  của chúng tôi tổ chức vào 18:00 29/10/2023 ! Khám phá hành trình của hai trái tim hòa quyện thành một ! Tìm hiểu thông tin về lễ cưới, tiệc và nhiều điều thú vị khác trên trang web đặc biệt của chúng tôi."
