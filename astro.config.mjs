0c92da043c299027378123884c509738b35c5ecd
kuro <anhddse02700@gmail.com>
2023-08-11 19:03:19 +0900

    fix meta

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import tailwind from "@astrojs/tailwind"
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://anh-an-wedding.vercel.app/",
   integrations: [mdx(), sitemap(), tailwind(), react()],
   experimental: {
-    assets: true
-  }
-});
+    assets: true,
+  },
+})
