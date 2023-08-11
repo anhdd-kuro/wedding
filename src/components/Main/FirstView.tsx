a08aab792c27fbd821d4a26898693e4d0bcb9c39
kuro <anhddse02700@gmail.com>
2023-08-11 23:06:52 +0900

    add map + icon

--- src/components/Main/FirstView.tsx
+++ src/components/Main/FirstView.tsx
@@ -5,10 +5,12 @@ import "swiper/css/autoplay"
 import "swiper/css/effect-fade"
 import "swiper/css/pagination"
 import "swiper/css/keyboard"
+import "./FirstView.css"
 
 export const FirstView = () => {
   return (
     <Swiper
+      id="main-slide"
       slidesPerView={1}
       onSlideChange={() => {
         console.log("slide change")
