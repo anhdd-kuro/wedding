2790c99152375342bb85ff9eb274060e5597b059
kuro <anhddse02700@gmail.com>
2023-08-12 10:08:00 +0900

    fix eng

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -24,19 +24,19 @@ const renderer: CountdownRendererFn = ({
       <strong className="flex-center mt-4 gap-x-[2rem] text-[4rem] font-medium tracking-wider text-black-light">
         <span className="flex-center flex-col">
           <span>{days}</span>
-          <span className="text-[1.2rem] text-green">ngày</span>
+          <span className="text-[1.2rem] text-green">days</span>
         </span>
         <span className="flex-center flex-col">
           <span>{hours}</span>
-          <span className="text-[1.2rem] text-green">giờ</span>
+          <span className="text-[1.2rem] text-green">hours</span>
         </span>
         <span className="flex-center flex-col">
           <span>{minutes}</span>
-          <span className="text-[1.2rem] text-green">phút</span>
+          <span className="text-[1.2rem] text-green">minutes</span>
         </span>
         <span className="flex-center flex-col">
           <span>{seconds}</span>
-          <span className="text-[1.2rem] text-green">giây</span>
+          <span className="text-[1.2rem] text-green">seconds</span>
         </span>
       </strong>
     )
