bd0f42070add30232bd1700971bfe70eee3f4866
kuro <anhddse02700@gmail.com>
2023-08-14 17:29:20 +0900

    add heartflow

--- src/utils.ts
+++ src/utils.ts
@@ -33,3 +33,9 @@ export function shuffleArray(array: any[]) {
 
   return newArray
 }
+
+export function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min) + min) // The maximum is exclusive and the minimum is inclusive
+}
