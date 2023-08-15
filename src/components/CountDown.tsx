e29c57112ed162ecf1d489cb5f568cfe242196b4
kuro <anhddse02700@gmail.com>
2023-08-15 22:29:57 +0900

    fix ja font-size

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import Countdown from "react-countdown"
 import type { CountdownRendererFn } from "react-countdown"
 import { useTranslations } from "@/i18n/utils"
+import clsx from "clsx"
 
 export const CustomCountDown: React.FC<{
   date: string | number | Date
@@ -24,25 +25,45 @@ const renderer =
         <strong className="flex-center mt-4 gap-x-[2rem] text-[3.2rem] font-medium tracking-wider text-black-light pc:text-[4rem]">
           <span className="flex-center flex-col">
             <span>{days}</span>
-            <span className="text-[1.2rem] text-green pc:text-[12px]">
+            <span
+              className={clsx(
+                "text-green pc:text-[12px]",
+                lang === "ja" ? "text-[1.5rem]" : "text-[1.2rem]"
+              )}
+            >
               {t("days")}
             </span>
           </span>
           <span className="flex-center flex-col">
             <span>{hours}</span>
-            <span className="text-[1.2rem] text-green pc:text-[12px]">
+            <span
+              className={clsx(
+                "text-green pc:text-[12px]",
+                lang === "ja" ? "text-[1.4rem]" : "text-[1.2rem]"
+              )}
+            >
               {t("hours")}
             </span>
           </span>
           <span className="flex-center flex-col">
             <span>{minutes}</span>
-            <span className="text-[1.2rem] text-green pc:text-[12px]">
+            <span
+              className={clsx(
+                "text-green pc:text-[12px]",
+                lang === "ja" ? "text-[1.4rem]" : "text-[1.2rem]"
+              )}
+            >
               {t("minutes")}
             </span>
           </span>
           <span className="flex-center flex-col">
             <span>{seconds}</span>
-            <span className="text-[1.2rem] text-green pc:text-[12px]">
+            <span
+              className={clsx(
+                "text-green pc:text-[12px]",
+                lang === "ja" ? "text-[1.4rem]" : "text-[1.2rem]"
+              )}
+            >
               {t("seconds")}
             </span>
           </span>
