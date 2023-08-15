d7a28764118d875fe3389ccd53a1703ba6e7146f
kuro <anhddse02700@gmail.com>
2023-08-15 22:17:21 +0900

    fix desceiption

--- src/consts.ts
+++ src/consts.ts
@@ -3,4 +3,4 @@
 
 export const SITE_TITLE = "A ♡ A・Wedding !"
 export const SITE_DESCRIPTION =
-  "Hãy cùng chia sẻ niềm hạnh phúc trong lễ cưới  của chúng tôi tổ chức vào 18:00 29/10/2023 tại khách sạn Deawoo Hà Nội ! Khám phá hành trình của hai trái tim hòa quyện thành một ! Tìm hiểu thông tin về lễ cưới, tiệc và nhiều điều thú vị khác trên trang web đặc biệt của chúng tôi."
+  "Hãy cùng chia sẻ niềm hạnh phúc trong lễ cưới của chúng tôi tổ chức vào 18:00 29/10/2023 tại khách sạn Deawoo Hà Nội ! Tra cứu thông tin về lễ cưới, tiệc và nhiều điều thú vị khác trên trang web đặc biệt của chúng tôi."
